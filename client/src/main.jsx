3b326b741b090afcc0d0d80762c7feb05783c5ba
Mani.Sh <100167532+Mani8217@users.noreply.github.com>
2025-02-26 13:04:55 +0100

    llevar codigos a repo nuevo

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import router from './router/router.jsx'
 import {RouterProvider} from'react-router-dom'
 import UserProvider from './context/UserContext.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
