02ef55d73b70233afdb486f477479437bf54445e
Mani.Sh <100167532+Mani8217@users.noreply.github.com>
2025-03-03 01:51:37 +0100

    some style corrections in two comp

--- client/src/components/AcademySection.jsx
+++ client/src/components/AcademySection.jsx
@@ -1,25 +1,23 @@
 import CustomComponent from "./CustomComponent";
-import starImg from "../assets/starnight.png"
-import logoImg from "../assets/Brown_Black_Modern_Elegant_Letter_AV_Logo.png"
+import { Link } from 'react-router-dom';
 
 export default function AcademySection() {
     return (
-        <div className="bg-pink-100 p-10">
+        <div style={{}}>
 
            <CustomComponent />
       
+            <div style={{backgroundColor:"#333333", height:"30px" , margin:"0 30px"}}></div>
 
-
-            
-            <div className="h-2 bg-black my-8"></div>
-
-               <div className="max-w-4xl mx-auto bg-white p-6 rounded-lg shadow-lg">
-                  <h3 className="text-xl font-bold text-left">
+             <div style={{display:"flex", flexDirection:"row", justifyContent:"space-around",padding:"50px 0" , backgroundColor:"white"}}>
+                  <h5 className="text-xl font-bold text-left">
                       ♦ CURSOS DISPONIBLES ♦
-                  </h3>
-                  <p className="text-orange-500 text-sm">Consulta la disponibilidad</p>
-                  <p className="text-gray-700 mt-4">
-                      Los cursos de <span className="font-bold">Archivos</span> están diseñados para estudiantes...
+                      <Link className="nav-link" to="/dashboard/pageBooks" style={{ color: "rgb(244, 174, 112)", fontSize: "0.6em", padding: '0 10px' }}>Consulta la disponibilidad</Link>
+
+                  </h5>
+                  
+                  <p style={{fontSize:"0.4em", width:"50%", padding: "10px"}}>
+                      Los cursos de <span className="font-bold">Archivos</span> Los cursos de ArteVivo están diseñados para los estudiantes que artes que se preocupa por el conocimiento que sus alumnos poniendo a su alcance gran cantidad de libros y cursos sin ningún costo. Los profesores que imparten las clases están altamente cualificados para garantizar el máximo aprovechamiento de cada uno de lxs alumnxs. 
                   </p>
               </div>
 
