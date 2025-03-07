032cd2497af0c8612a95526eb63861ecee73439e
Nho89 <noa1720@gmail.com>
2025-03-07 15:14:38 +0100

    fix: force com back commit

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -15,9 +15,9 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'username', 'email', 'password', 'role', 'first_name', 'last_name']
         extra_kwargs = {'password': {'write_only': True}}
-    # def create(self, validated_data):
-    #     validated_data['password'] = make_password(validated_data['password']) 
-    #     return super().create(validated_data)   
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password']) 
+        return super().create(validated_data)   
 
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
